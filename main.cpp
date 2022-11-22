e601670db2939a5dbc7031eabb09775704f27f56
Angyal Ádám Szilárd <107045792+AASZ40@users.noreply.github.com>
2022-11-22 16:04:41 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,27 +4,60 @@
 
 using namespace std;
 
+
 int main() {
     string line;
     string dontes;
     string A = "IGEN";
     string B = "NEM";
 
+    /*fstream newfile;
+    newfile.open("C:\c++\mosze_2022_game\Mosze-2022-jatek\Story\bevezeto.txt", ios::in);*/
 
-
-    ifstream file("bevezeto.txt");
+    ifstream file("bevezeto.txt", ios::out);
     if (file.is_open()) {
         while (getline(file, line)) {
             cout << line << endl;
         }
         file.close();
     }
-    else
-        cout << "Unable to open file";
-    cout << endl << "Mi a valaszod?" << endl;
+    else {
+        cout << "Nem nyithato meg a fajl!";
 
-    cin >> dontes;
-    cout << endl;
+        cout << endl << "El akarod kezdeni a jatekot?" << endl;
+
+        cin >> dontes;
+        cout << endl;
+    }
+
+    if (dontes == A) {
+        ifstream file("terulet1.txt", ios::out);
+        if (file.is_open()) {
+            while (getline(file, line)) {
+                cout << line << '\n';
+            }
+            file.close();
+        }
+        else
+            cout << "Nem nyithato meg a fajl!";
+    }
+
+    else if (dontes == B) {
+        ifstream file("bevezeto.txt", ios::out);
+        if (file.is_open()) {
+            while (getline(file, line)) {
+                cout << line << endl;
+            }
+            file.close();
+        }
+        else
+            cout << "Nem nyithato meg a fajl!";
+
+        cout << endl << "El akarod kezdeni a jatekot?" << endl;
+
+        cin >> dontes;
+        cout << endl;
+    }
 
 
 
